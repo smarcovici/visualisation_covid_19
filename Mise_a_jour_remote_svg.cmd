964ff1f4e86d0497bdd36af312812b8afdf54134
smarcovici <smarcovici@netcourrier.com>
2021-01-08 14:06:43 +0100

    Repertoire

--- Mise_a_jour_remote_svg.cmd
+++ Mise_a_jour_remote_svg.cmd
@@ -1,4 +1,4 @@
 cd C:\Users\sylvain_2\Documents\Python Scripts\Beyond_basics\Covid_19\Covid_19\Surveillance_deconfinement\Images
 git add .
-if (%1 == %1) (git commit -m "repertoire") else (git commit -m "teste")
+if (%1 == %1) (git commit -m %1) else (git commit -m "Repertoire")
 git push -u origin master
