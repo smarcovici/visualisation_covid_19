7575e330c7306be935dbb0df1e0626edaff755df
smarcovici <smarcovici@netcourrier.com>
2021-01-08 12:54:32 +0100

    nouvelle version

---
+++ envoi_alertes_2.cmd
@@ -0,0 +1,4 @@
+cd C:\Users\sylvain_2\Documents\Python Scripts\Beyond_basics\Covid_19\Covid_19\Surveillance_deconfinement\Images
+git add "Alertes_entrees_recentes_2.png"
+git commit -m "nouvelle version"
+git push -u origin master
