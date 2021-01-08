618d755983a189779535f0715f644fa655e2775f
smarcovici <smarcovici@netcourrier.com>
2021-01-08 12:58:41 +0100

    nouvelle version

--- Mise_a_jour_remote.cmd
+++
@@ -1,4 +0,0 @@
-cd C:\Users\sylvain_2\Documents\Python Scripts\Beyond_basics\Covid_19\Covid_19\Surveillance_deconfinement\Images
-git add .
-git commit -m "nouvelle version"
-git push -u origin master
