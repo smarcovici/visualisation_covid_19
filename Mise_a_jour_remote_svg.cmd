f6d6e6484fc6787f573fc469293f5f72d7d926f7
smarcovici <smarcovici@netcourrier.com>
2021-01-08 14:23:35 +0100

    test 4

--- Mise_a_jour_remote_svg.cmd
+++ Mise_a_jour_remote_svg.cmd
@@ -1,4 +1,5 @@
+set ARG=%1
 cd C:\Users\sylvain_2\Documents\Python Scripts\Beyond_basics\Covid_19\Covid_19\Surveillance_deconfinement\Images
 git add .
-if (%1 == %1) (git commit -m %1) else (git commit -m "Repertoire")
+if not defined ARG (git commit -m "test 4") else (git commit -m %1)
 git push -u origin master
